01c264d9aca3416ca30ac1cc32ea3f676e23b79c
Leonardo Licen <leolicen@yahoo.it>
2026-01-12 12:00:14 +0100

    add course service, endpoint and dependency

---
+++ app/routers/course.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+import uuid
+from fastapi import APIRouter, Depends
+from ..models.course import CourseInDB
+from ..models.student import StudentPublic
+from ..dependencies import get_current_student, get_course_service
+from ..services.course import CourseService
+
+
+
+# definisco router /courses 
+router = APIRouter(
+    prefix="/courses",
+    tags=["courses"],
+)
+
+# -- GET STUDENT COURSE --
+# endpoint PROTETTO (utenti attivi e inattivi)
+@router.get("/", response_model=CourseInDB)
+def get_student_course(
+    current_student: Annotated[StudentPublic, Depends(get_current_student)],
+    course_service: Annotated[CourseService, Depends(get_course_service)]
+    ):
+    return course_service.get_student_course(current_student.course_id)
