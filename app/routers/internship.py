39fd62b93f61fc8842cbb43eadbd6bc85b0db5c7
Leonardo Licen <leolicen@yahoo.it>
2026-01-15 16:22:47 +0100

    add further validation and attended_hours update logic to create and delete entries endpoints and service

--- app/routers/internship.py
+++ app/routers/internship.py
@@ -54,39 +54,54 @@ def create_internship_entry(
     internship_service: Annotated[InternshipService, Depends(get_internship_service)],
     entry: InternshipEntryCreate
 ):
-    # check in più su agreement_id
-    if entry.agreement_id != agreement_id or not internship_service.student_owns_specific_agreement(current_active_student.student_id, entry.agreement_id):
+   # check se entry appartiene a agreement
+    if entry.agreement_id != agreement_id:
         raise HTTPException(
-            status_code=403,
-            detail="Agreement not found or student not authorized. Cannot create entry."
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Entry agreement mismatch"
         )
     
-    return internship_service.create_internship_entry(entry)
+    # check corrispondenza studente <-> active agreement
+    if not internship_service.student_owns_specific_active_agreement(current_active_student.student_id, agreement_id):
+        raise HTTPException(
+            status_code=403,
+            detail="Agreement not owned or inactive. Cannot create entry."
+        )
+    
+    return internship_service.create_internship_entry(agreement_id, entry)
 
 
 
 # -- DELETE INTERNSHIP ENTRY --
-@router.delete("/{agreement_id}/entries/{entry_id}")
+@router.delete("/{agreement_id}/entries/{entry_id}", response_model=dict[str, str])
 def delete_internship_entry(
     agreement_id: uuid.UUID,
     entry_id: uuid.UUID,
     current_active_student: Annotated[StudentPublic, Depends(get_current_active_student)],
     internship_service: Annotated[InternshipService, Depends(get_internship_service)]
 ):
-    if not internship_service.student_owns_specific_agreement(current_active_student.student_id, agreement_id):
+    # check corrispondenza studente <-> agreement + agreement attivo/inattivo 
+    if not internship_service.student_owns_specific_active_agreement(current_active_student.student_id, agreement_id):
         print(f"Agreement does not match student id.")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Agreement not found. Cannot delete entry."
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Agreement not owned or inactive. Cannot delete entry."
         )
-        
-    agreem_id = internship_service.get_entry_agreement_id_by_entry_id(entry_id)
+    # estraggo agreement_id da entry da eliminare
+    entry_agreem_id = internship_service.get_entry_agreement_id_by_entry_id(entry_id)
     
-    if agreem_id != agreement_id:
-        print(f"Agreement does not match entry. Cannot delete entry.")
+    if entry_agreem_id is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Entry not found for this agreement"
+            detail="Entry not found."
+        )
+    
+    # check entry belongs to agreement
+    if entry_agreem_id != agreement_id:
+        print(f"Entry does not belong to this agreement. Cannot delete entry.")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Entry not found. Cannot delete entry."
         )
         
     
