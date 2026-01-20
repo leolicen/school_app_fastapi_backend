2ace2cf7cc2e2bf388fdff8bfd5e46963f294512
Leonardo Licen <leolicen@yahoo.it>
2026-01-20 12:21:23 +0100

    enhance course service with log and 'with' block

--- app/services/course.py
+++ app/services/course.py
@@ -2,6 +2,9 @@ import uuid
 from fastapi import HTTPException, status
 from sqlmodel import Session, select
 from ..models.course import CourseInDB, CoursePublic
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class CourseService():
@@ -10,13 +13,16 @@ class CourseService():
         
     # -- GET STUDENT COURSE --
     def get_student_course(self, course_id: uuid.UUID) -> CoursePublic:
-        # seleziono corso in base a id
-        get_course_stmt = select(CourseInDB).where(
-            CourseInDB.course_id == course_id
-        )
-        student_course: CourseInDB | None = self._db.exec(get_course_stmt).first()
+        
+        with self._db:
+            # select course by id 
+            get_course_stmt = select(CourseInDB).where(
+                CourseInDB.course_id == course_id
+            )
+            student_course: CourseInDB | None = self._db.exec(get_course_stmt).first()
         
         if not student_course:
+            logging.warning(f"Course {course_id} not found")
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Course not found"
