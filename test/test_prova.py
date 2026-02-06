49eb523580c369f3f147aafb93bf56298cf295ad
Leonardo Licen <leolicen@yahoo.it>
2026-02-06 12:16:28 +0100

    create access token fixture, modify auth header fixture

--- test/test_prova.py
+++ test/test_prova.py
@@ -1,14 +1,18 @@
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from app.models.student import StudentInDB
 
 
-def test_get_current_student_with_valid_token(client: TestClient, auth_header):
+@pytest.mark.anyio
+async def test_get_current_student_with_valid_token(async_client: AsyncClient, auth_header):
     
-    response = client.get("/students/me", headers=auth_header)
+    response = await async_client.get("/students/me", headers=auth_header)
     
     assert response.status_code == 200
     assert response.json()["surname"] == "Doe"
+    
     
 
 
