c224c3dd77124aee67149d7c3a08593707d25aa3
Leonardo Licen <leolicen@yahoo.it>
2026-02-07 16:33:47 +0100

    set filter to logger handler, improve first tests with print

--- test/integration/routers/test_student.py
+++ test/integration/routers/test_student.py
@@ -1,11 +1,21 @@
 from httpx import AsyncClient
-from fastapi.testclient import TestClient
+import logging
+import json
+
+
+logger = logging.getLogger(__name__)
+
 
 
 
 async def test_get_current_student_with_valid_token(async_client: AsyncClient, auth_header):
     
     response = await async_client.get("/students/me", headers=auth_header)
+    
+    print("\n")
+    print("Response model:")
+    print(json.dumps(response.json(), indent=2, ensure_ascii=False))
+    print("\n")
     
     assert response.status_code == 200
     assert response.json()["surname"] == "Doe"
