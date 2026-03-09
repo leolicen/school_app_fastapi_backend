66976d4ad897d5bd1ac3c676792df21dc2e2c643
Leonardo Licen <leolicen@yahoo.it>
2026-03-09 16:01:35 +0100

    modify comments and format style in services folder

--- app/services/course.py
+++ app/services/course.py
@@ -11,40 +11,32 @@ logger = logging.getLogger(__name__)
 
 
 class CourseService():
+
     def __init__(self, session: Session):
         self._db = session
-        
-        
-    # -- GET COURSES LIST --
+
+
     def get_courses_list(self) -> List[CourseListPublic]:
-        """ 
-        Retrieves a list of available courses (active courses only).
-        """
-        
+        """Retrieve a list of available courses (active only)."""
         courses_list: Sequence[Tuple[uuid.UUID, str]] = self._db.exec(
             select(CourseInDB.course_id, CourseInDB.name).where(
-                CourseInDB.is_active == True 
+                CourseInDB.is_active == True
             )
         ).all()
-        
+
         return [CourseListPublic(course_id=course_id, name=name) for course_id, name in courses_list]
-    
-    
-        
-    # -- GET STUDENT COURSE --
+
+
     def get_student_course(self, course_id: uuid.UUID) -> CoursePublic:
-        """ 
-        Retrieves the course attended by the authenticated student.
-        """
-        
-        # select course by id 
+        """Retrieve the course attended by the authenticated student."""
+        # select course by id
         get_course_stmt = select(CourseInDB).where(
             CourseInDB.course_id == course_id
         )
         student_course: CourseInDB | None = self._db.exec(get_course_stmt).first()
-        
+
         if not student_course:
             logger.warning(f"Course {course_id} not found")
             raise CourseNotFoundError()
-        
+
         return CoursePublic.model_validate(student_course)
