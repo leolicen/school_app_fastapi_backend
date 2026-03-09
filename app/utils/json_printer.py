b68f92ed5e36223a54ef40ab10ef1b458003038b
Leonardo Licen <leolicen@yahoo.it>
2026-03-09 12:42:39 +0100

    modify comments and format style in utils folder

--- app/utils/json_printer.py
+++ app/utils/json_printer.py
@@ -1,23 +1,22 @@
 import json
 
 
-
 def print_json_response(json_response, response_name: str, indentation: int = 2, separator: bool = True):
-    """ Print a clearly formatted JSON response.
+    """Print a clearly formatted JSON response.
 
-        Args: 
-            json_response: Dictionary or JSON serializable object
-            response_name: Name of the specific response
+    Args:
+        json_response: Dictionary or JSON serializable object
+        response_name: Name of the specific response
     """
     try:
         if separator:
             print("\n" + "="*50)
-            
+
         print(f"\n{response_name}:\n")
         print(json.dumps(json_response, indent=indentation, ensure_ascii=False))
-        
+
         if separator:
             print("\n" + "="*50)
-        
+
     except TypeError:
         print(f"Object not serializable: {json_response}")
