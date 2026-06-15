75f12051c4922239aed1666169100d056e1ba345
Leonardo Licen <leolicen@yahoo.it>
2026-06-15 10:24:41 +0200

    add test tutor

---
+++ test/integration/routers/tutor/test_tutor.py
@@ -0,0 +1,29 @@
+import logging
+
+from httpx import AsyncClient
+
+from app.utils.json_printer import print_json_response
+
+
+logger = logging.getLogger(__name__)
+
+
+
+
+async def test_get_current_tutor_with_valid_token(async_client: AsyncClient, tutor_auth_header):
+    
+    response = await async_client.get("/tutors/me", headers=tutor_auth_header)
+    
+    print_json_response(json_response=response.json(), response_name="Current tutor profile")
+    
+    assert response.status_code == 200
+    assert response.json()["surname"] == "Patarnalli"
+
+# without token
+
+async def test_get_current_tutor_with_invalid_token(async_client: AsyncClient):
+    
+    response = await async_client.get("/tutors/me", headers={"Authorization": "Bearer dsvjkbcjcsdkh"})
+    
+    assert response.status_code == 401
+    
